a2b2a40973c414f3de125e663c7eb5d742614344
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2022-03-21 13:57:02 -0400

    added laoding slider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,37 @@
 import { motion } from "framer-motion";
 import GlobalStyles from "../styles/GlobalStyle.js";
 import Layout from "../components/Layout";
-function MyApp({ Component, pageProps, router }) {
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import "../public/nprogress.css";
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <>
       <GlobalStyles />
-
       <Layout>
         <motion.div
           key={router.route}
@@ -26,5 +52,3 @@ function MyApp({ Component, pageProps, router }) {
     </>
   );
 }
-
-export default MyApp;
